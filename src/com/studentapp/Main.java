bff766c0df4bf152696d1566d43d4fb54473a6e3
Purushoth_Suresh <purushothsuresh11@gmail.com>
2024-12-05 16:27:39 +0530

    Removed unused classes

--- src/com/studentapp/Main.java
+++ src/com/studentapp/Main.java
@@ -1,29 +1,150 @@
 package com.studentapp;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+	
+	private static List<Student> studentList;
+	private static Scanner scanner;
 	
 	public static void main(String[] args) {
 		System.out.println("********* Student Management System *********");
+		studentList = new ArrayList<Student>();
+		scanner = new Scanner(System.in);
+		while(true) {
 		System.out.println("********* Welcome *********");
+		System.out.println("Select an Option....");
+		System.out.println("1. Register a Student");
+		System.out.println("2. Find Student with studentID");
+		System.out.println("3. List all student information");
+		System.out.println("4. List Student information in sorted order");
+		System.out.println("5. Exit");
+		int option = scanner.nextInt();
 		
-		Student s1;
-		s1 = new Student("Kira Agar", 22, "S-1");
-		s1.enrollCourse("Java");
-		s1.enrollCourse("Devops");
-		s1.enrollCourse("DSA");
-//		s1.enrollCourse("C#");
+		switch(option) {
+		case 1: 
+			enrollStudent(scanner);
+			break;
+		case 2:
+			findStudentById(scanner);
+			break;
+		case 3:
+			printAllStudentData();
+			break;
+		case 4:
+			sortByName();
+			break;
+		case 5:
+			exit();
+			break;
+		default:
+			System.out.println("Invalid option selected!...Enter between 1 to 5");
+		}
 		
-		System.out.println(s1);
-//		s1.printStudentInfo();
+		}
 		
-		Student s2 = new Student("Uday", 23, "S-11");
-		s2.enrollCourse("Devops");
-		System.out.println(s2);
+	}
+	
+	private static void exit() {
+		System.out.println("Good Bye");
+		System.exit(0);
+	}
+
+	private static void printAllStudentData() {
 		
-		Student s3 = new Student("Uday", 23, "S-21");
-		s3.enrollCourse("DSA");
-		System.out.println(s3);
+		if(studentList.size()>0) {
 		
+		System.out.println("-------------- Printing All Student Data --------------");
+		
+		for (Student student : studentList) {
+			student.printStudentInfo();
+		}
+
+		System.out.println("-------------- ******************* --------------");
+		}
+		else {
+			System.err.println("Student List is Empty!! No Student Recorde Founded");
+		}
+	}
+
+	private static void findStudentById(Scanner scanner2) {
+		Student studentFound = null;
+		System.out.println("Enter the Student Id");
+		String studentId = scanner2.next();
+		try {
+			studentFound = studentList.stream().filter(student -> student.getStudentId().equalsIgnoreCase(studentId))
+		.findFirst()
+		.orElseThrow(() -> new RuntimeException("No Data Found!!!"));
+		} 
+		catch(RuntimeException e) {
+			System.err.println("Student with ID "+studentId+ " not found!!");
+		}
+		
+		studentFound.printStudentInfo();
+		
+	}
+
+	private static void enrollStudent(Scanner scanner2) {
+		System.out.println("Enter the Student Name");
+		String studentName = scanner2.next();
+		
+		System.out.println("Enter the Student age");
+		int studentAge = scanner2.nextInt();
+		
+		System.out.println("Enter the Student Id");
+		String studentId = scanner2.next();
+		
+		Student newStudent = new Student(studentName, studentAge, studentId); 
+		studentList.add(newStudent);
+		while(true) {
+		System.out.println("Enter the course to be enrolled!!..");
+		String courseName = scanner2.next();
+		if(courseName.equalsIgnoreCase("done")) {
+			break; //exit loop
+		}
+		newStudent.enrollCourse(courseName);
+	 }
+	newStudent.printStudentInfo();
+   }
+
+	private static void sortByName() {
+		
+//		Without Lambda expression 
+		
+//		Comparator<Student> studentNameComparator = new Comparator<Student>() {
+//			@Override
+//			public int compare(Student o1, Student o2) {
+//				return o1.getName().compareTo(o2.getName());
+//			}
+//		};
+		
+//		With Lambda expression
+		
+		Comparator<Student> studentNameComparator = (o1,o2) -> o1.getName().compareTo(o2.getName());
+		
+		Collections.sort(studentList,studentNameComparator);
+		printAllStudentData();
+		
+	}
+
+	public static Student findStudentById(String studentId) {
+		
+		Student result = null;
+		
+		try {
+		result = studentList.stream().filter(x -> x.getStudentId().equalsIgnoreCase(studentId))
+		.findFirst()
+		.orElseThrow(() -> new RuntimeException("No Data Found!!!"));
+		} 
+		catch(RuntimeException e) {
+			System.err.println("Student with ID "+studentId+ " not found!!");
+		}
+		
+		return result;
 	}
 
 }
